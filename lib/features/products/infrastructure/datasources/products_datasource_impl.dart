7182f436e0346b79e72768078e9ee1638d93bf5a
Javier García <a2027garciaj@gmail.com>
2023-11-06 20:32:07 +0100

    añadido creación y modificación de productos

--- lib/features/products/infrastructure/datasources/products_datasource_impl.dart
+++ lib/features/products/infrastructure/datasources/products_datasource_impl.dart
@@ -2,6 +2,7 @@ import 'package:dio/dio.dart';
 import 'package:teslo_shop/config/config.dart';
 import 'package:teslo_shop/features/products/domain/datasources/products_datasource.dart';
 import 'package:teslo_shop/features/products/domain/entities/product.dart';
+import 'package:teslo_shop/features/products/infrastructure/errors/product_errors.dart';
 import 'package:teslo_shop/features/products/infrastructure/mappers/product_mapper.dart';
 
 class ProductsDatasourceImpl extends ProductsDatasource {
@@ -21,27 +22,59 @@ class ProductsDatasourceImpl extends ProductsDatasource {
   );
 
   @override
-  Future<Product> createUpdateProduct(Map<String, dynamic> productLike) {
-    // TODO: implement createUpdateProduct
-    throw UnimplementedError();
+  Future<Product> createUpdateProduct(Map<String, dynamic> productLike) async {
+    
+    try {
+      
+      final String? productId = productLike['id'];
+      final String method = (productId == null) ? 'POST':'PATCH';
+      final String url = (productId == null) ? '/products': '/products/$productId';
+      
+      productLike.remove('id');
+
+      final response = await dio.request(
+        url,
+        data: productLike,
+        options: Options(
+          method: method,
+        )
+      );
+      final product = ProductMapper.jsonToEntity(response.data);
+      return product;
+
+    } catch (e) {
+      throw Exception();
+    }
+
   }
 
   @override
-  Future<Product> getProductById(String id) {
-    // TODO: implement getProductById
-    throw UnimplementedError();
+  Future<Product> getProductById(String id) async {
+    try {
+      final response = await dio.get('/products/$id');
+
+      final product = ProductMapper.jsonToEntity(response.data);
+
+      return product;
+      
+    } on DioException catch (e) {
+      if (e.response!.statusCode == 404) throw ProductNotFound();
+      throw Exception();
+    } catch (e) {
+      throw Exception();
+    } 
+
   }
 
   @override
   Future<List<Product>> getProductsByPage({int limit = 10, int offset = 0}) async {
     final response = await dio.get<List>('/products?limit=$limit&offset=$offset');
     
-    // final List<Product> products = (response.data ?? []).map((product) => ProductMapper.jsonToEntity(product)).toList() as List<Product>;
-
     final List<Product> products = [];
     for (final product in response.data ?? []) {
       products.add( ProductMapper.jsonToEntity(product)); 
     }
+    
     return products;
   }
 
--- lib/features/products/infrastructure/infrastructure.dart
+++ lib/features/products/infrastructure/infrastructure.dart
@@ -1,3 +1,4 @@
 export 'datasources/products_datasource_impl.dart';
-export 'respositories/products_repository_impl.dart';
+export 'errors/product_errors.dart';
 export 'mappers/product_mapper.dart';
+export 'respositories/products_repository_impl.dart';
